883dfc4eb153e3755051b8c57f6aab396db78e0c
Aman Sharma <amanrazdan@amp.dev>
2024-11-01 17:58:49 -0700

    added flow section

--- next.config.mjs
+++ next.config.mjs
@@ -208,6 +208,9 @@ const permanentRedirects = [
     // ['/docs/Reports','https://lamatic.ai/docs/reports'],
     ['/workflow','https://lamatic.ai/docs/flows'],
     ['/aboutus','https://lamatic.ai/about-us'],
+    ['/docs/langfuse-integration','https://www.lamatic.ai/docs/reports/langfuse-integration'],
+    ['/docs/flows/trigger','https://www.lamatic.ai/docs/flows'],
+    ['/docs/flows/low-code_config','/docs/flow_config'],
 
 
 

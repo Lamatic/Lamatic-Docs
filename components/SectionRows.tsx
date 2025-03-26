0cec9d4f859e9996906d901ad2ed63317e3d41d0
d-pamneja <dpamneja@gmail.com>
2025-03-26 18:30:22 +0530

    Feat : Add Tags

--- components/SectionRows.tsx
+++ components/SectionRows.tsx
@@ -2,24 +2,23 @@ import React from 'react';
 
 const SectionRows = ({ section, chips, className = '' }) => {
   return (
-    <div className={`flex items-baseline space-x-2 ${className}`}>
-      <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
+    <div className={`flex items-baseline space-x-1 ${className}`}>
+      <h4 className="text-lg font-semibold text-gray-800 dark:text-gray-200">
         {section}
-      </h3>
+      </h4>
       <div className="flex items-baseline gap-2">
         {chips.map((chip, index) => (
           <span
             key={index}
             className="
-              px-3 py-1
-              text-sm
+              px-3 py-2
+              text-xs
               rounded-full
               bg-gradient-to-r
-              from-red-100 to-red-200
-              text-red-500
-              font-semibold
+              from-blue-100 to-blue-200
+              text-blue-500
               inline-flex items-center justify-center
-              hover:from-red-200 hover:to-red-300
+              hover:from-blue-200 hover:to-blue-300
               transition-all
               duration-300
               cursor-default

f260f743fcb8bc24bf4564dfe0c83336e9d9e543
vrijraj <vrijraj2396@gmail.com>
2025-09-10 18:03:01 +0800

    Refactor agent kits structure: rename 'Agent Kits' to 'Templates', update routing and metadata accordingly, and remove deprecated files for improved organization and clarity.

--- theme.config.tsx
+++ theme.config.tsx
@@ -26,6 +26,7 @@ import {
   GraduationCap,
   Blocks,
   BotMessageSquare,
+  LayoutTemplate
 } from "lucide-react";
 import {
   AvailabilityBanner,
@@ -61,7 +62,7 @@ const config: DocsThemeConfig = {
             {[
               { title: "Docs", path: "/docs", Icon: LibraryBig },
               { title: "Integrations", path: "/integrations", Icon: Blocks },
-              { title: "Agent Kits", path: "/agentkits", Icon: BotMessageSquare },
+              { title: "Templates", path: "/templates", Icon: LayoutTemplate },
               { title: "Guides", path: "/guides", Icon: GraduationCap },
               { title: "Book a demo", path: "/docs/demo", Icon: Phone },
               {
@@ -137,7 +138,7 @@ const config: DocsThemeConfig = {
           ? "%s - Lamatic.ai Integrations"
           : asPath.startsWith("/ambassadors")
           ? "%s - Lamatic.ai Ambassador"
-          : asPath.startsWith("/agentkits/")
+          : asPath.startsWith("/templates/")
           ? "%s - Lamatic.ai Agent Kits"
           : asPath.startsWith("/security/")
           ? "%s - Lamatic.ai Security"
@@ -162,7 +163,7 @@ const config: DocsThemeConfig = {
       ? "Changelog"
       : asPath.startsWith("/cookbook/")
       ? "Cookbook"
-      : asPath.startsWith("/agentkits/")
+      : asPath.startsWith("/templates/")
       ? "Agent Kits"
       : "";
 

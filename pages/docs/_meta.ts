cc6f0ab5e8496ce2bec2a7aeb29a5aea0f5f52ba
vrijraj <vrijraj2396@gmail.com>
2026-03-22 14:54:45 +0700

    Refactor documentation structure and remove obsolete files
    - Updated the _meta.ts file to enhance organization and clarity, including renaming sections and adding new entries.
    - Removed outdated documentation files: all-integrations.mdx, all-nodes.mdx, node-assistant.mdx, and node-logic.mdx, along with their associated images.
    - Introduced new entries for "Node Assistant" and "Node Logic" in the nodes metadata for better navigation.
    - Adjusted titles and descriptions for improved consistency across documentation.

--- pages/docs/_meta.ts
+++ pages/docs/_meta.ts
@@ -11,11 +11,11 @@ export default {
     title: "Quickstart (5 min)",
     type: "doc",
   },
-  "architecture":{
+  architecture: {
     title: "Architecture",
     type: "doc",
   },
-  
+
   glossary: {
     title: "Glossary",
     type: "doc",
@@ -24,10 +24,10 @@ export default {
     title: "Concepts",
     type: "doc",
   },
-  
-  "-- Build": {
+
+  BUILD: {
     type: "separator",
-    title: "🛠️ Build",
+    title: "BUILD",
   },
   Changelog: {
     title: "Changelog",
@@ -45,14 +45,6 @@ export default {
     title: "Nodes",
     type: "doc",
   },
-  "node-assistant": {
-    title: "Node Assistant",
-    type: "doc",
-  },
-  ide: {
-    title: "IDE",
-    type: "doc",
-  },
   agents: {
     title: "Agents",
     type: "doc",
@@ -61,12 +53,16 @@ export default {
     title: "Models",
     type: "doc",
   },
+  // "data":{
+  //   title: "Data",
+  //   type: "doc",
+  // },
   context: {
     title: "Data",
     type: "doc",
   },
-  interface: {
-    title: "Interface",
+  ide: {
+    title: "IDE",
     type: "doc",
   },
   "mcp-tools": {
@@ -77,42 +73,71 @@ export default {
     title: "Test",
     type: "doc",
   },
-  "version-control": {
-    title: "Version Control",
-    type: "doc",
-  },
-  environment: {
-    title: "Environment",
-    type: "doc",
-  },
-  templates: {
-    title: "Templates",
-    type: "doc",
-  },
   "vibe-build": {
     title: "Vibe Build",
     type: "doc",
   },
-  "-- deploy": {
+
+  // interface: {
+  //   title: "Interface",
+  //   type: "doc",
+  // },
+
+  // templates: {
+  //   title: "Templates",
+  //   type: "doc",
+  // },
+
+  DEPLOY: {
     type: "separator",
-    title: "🚀 Deploy",
+    title: "DEPLOY",
   },
   deployments: {
     title: "Deployments",
     type: "doc",
   },
+  environment: {
+    title: "Environment & Variables",
+    type: "doc",
+  },
+  "version-control": {
+    title: "Version Control",
+    type: "doc",
+  },
   jobs: {
-    title: "Jobs",
+    title: "Jobs & Scheduling",
+    type: "doc",
+  },
+  //
+
+  // ===================
+  INTEGRATE: {
+    type: "separator",
+    title: "INTEGRATE",
+  },
+  "api-overview": {
+    title: "API Overview",
+    type: "doc",
+  },
+  "flow-integration": {
+    title: "Flow Integration",
+    type: "doc",
+  },
+
+  interface: {
+    title: "GraphQL, Webhooks & Widgets",
     type: "doc",
   },
   "api-integration": {
-    title: "API Integration",
+    title: "SDK",
     type: "doc",
   },
-  "-- optimize": {
+  // ===================
+  MONITOR: {
     type: "separator",
-    title: "🧩 Optimize",
+    title: "MONITOR",
   },
+
   logs: {
     title: "Logs",
     type: "doc",
@@ -121,144 +146,132 @@ export default {
     title: "Reports",
     type: "doc",
   },
-  "node-logic": {
-    title: "Node Logic",
-    type: "doc",
-  },
+  // "node-logic": {
+  //   title: "Node Logic",
+  //   type: "doc",
+  // },
   "feedback-api": {
     title: "Feedback API",
     type: "doc",
   },
-  "-- references": {
+  "REFERENCES": {
     type: "separator",
-    title: "References",
+    title: "REFERENCES",
   },
-  "api-ref": {
-    title: "GraphQL API ↗",
-    href: "https://graphql.org/learn/",
-    newWindow: true,
-  },
-  Langfuse: {
-    title: "Langfuse ↗",
-    href: "https://langfuse.com/docs",
-    newWindow: true,
-  },
-  cloudflare: {
-    title: "Cloudflare Workers ↗",
-    href: "https://workers.cloudflare.com/",
-    newWindow: true,
-  },
-  Posthog: {
-    title: "PostHog ↗",
-    href: "https://posthog.com/docs",
-    newWindow: true,
-  },
-  "weaviate-ref": {
-    title: "Weaviate Reference ↗",
-    href: "https://weaviate.io/developers/weaviate/introduction",
-    newWindow: true,
-  },
-  "roadmap-ref": {
-    title: "Roadmap ↗",
-    href: "https://product.lamatic.ai/roadmap",
-    newWindow: true,
-  },
-  "-- more": {
-    type: "separator",
-    title: "More",
-  },
-  status: {
-    title: "Platform Status ↗",
-    href: "https://status.lamatic.ai",
-    newWindow: true,
-  },
+  
   contributing: {
     title: "Contributing",
     type: "doc",
   },
-  Trash: {
-    title: "Privacy Policy",
-    type: "page",
+  "nodes-ref": {
+    "title": "All Nodes (A-Z)",
+    "href": "/docs/nodes",
+    "newWindow": false
+  },
+  "integrations-ref": {
+    "title": "All Integrations (A-Z)",
+    "href": "/integrations",
+    "newWindow": false
+  },
+
+  // "roadmap-ref": {
+  //   title: "Roadmap ↗",
+  //   href: "https://product.lamatic.ai/roadmap",
+  //   newWindow: true,
+  // },
+  // "-- more": {
+  //   type: "separator",
+  //   title: "More",
+  // },
+  // status: {
+  //   title: "Platform Status ↗",
+  //   href: "https://status.lamatic.ai",
+  //   newWindow: true,
+  // },
+  
+  // Trash: {
+  //   title: "Privacy Policy",
+  //   type: "page",
+  //   display: "hidden",
+  // },
+  "vulnerability-disclosure": {
+    title: "Vulnerability Disclosure",
+    type: "doc",
     display: "hidden",
   },
-  "vulnerability-disclosure": {
-    "title": "Vulnerability Disclosure",
-    "type": "doc",
-    "display": "hidden"
+  "report-issue": {
+    title: "Confidential Reporting",
+    type: "doc",
+    display: "hidden",
   },
-  "report-issue":{
-    "title": "Confidential Reporting",
-    "type": "doc",
-    "display": "hidden"
+  "privacy-policy": {
+    title: "Privacy Policy",
+    type: "doc",
+    display: "hidden",
   },
-  "privacy-policy":{
-    "title": "Privacy Policy",
-    "type": "doc",
-    "display": "hidden"
+  "cookie-policy": {
+    title: "Cookie Policy",
+    type: "doc",
+    display: "hidden",
   },
-  "cookie-policy":{
-    "title": "Cookie Policy",
-    "type": "doc",
-    "display": "hidden"
+  "data-deletion-policy": {
+    title: "Data Deletion Policy",
+    type: "doc",
+    display: "hidden",
   },
-  "data-deletion-policy":{
-    "title": "Data Deletion Policy",
-    "type": "doc",
-    "display": "hidden"
+  "terms-of-service": {
+    title: "Terms of Service",
+    type: "doc",
+    display: "hidden",
   },
-  "terms-of-service":{
-    "title": "Terms of Service",
-    "type": "doc",
-    "display": "hidden"
+  hacktoberfest: {
+    title: "Hacktoberfest",
+    type: "doc",
+    display: "hidden",
   },
-  "hacktoberfest":{
-    "title": "Hacktoberfest",
-    "type": "doc",
-    "display": "hidden"
+  career: {
+    title: "Career",
+    type: "doc",
+    display: "hidden",
   },
-  "career":{
-    "title": "Career",
-    "type": "doc",
-    "display": "hidden"
+  brandkit: {
+    title: "Brandkit",
+    type: "doc",
+    display: "hidden",
   },
-  "brandkit":{
-    "title": "Brandkit",
-    "type": "doc",
-    "display": "hidden"
+  partners: {
+    title: "Partners",
+    type: "doc",
+    display: "hidden",
   },
-  "partners":{
-    "title": "Partners",
-    "type": "doc",
-    "display": "hidden"
+  security: {
+    title: "Security",
+    type: "doc",
+    display: "hidden",
   },
-  "security":{
-    "title": "Security",
-    "type": "doc",
-    "display": "hidden"
-  },
-  "slack":{
-    "title": "Slack",
-    "type": "doc",
-    "display": "hidden"
+  slack: {
+    title: "Slack",
+    type: "doc",
+    display: "hidden",
   },
   legal: {
-    "title": "Legal",
-    "type": "doc",
-    "display": "hidden"
+    title: "Legal",
+    type: "doc",
+    display: "hidden",
   },
-  "config-schema":{
-    "title": "Config Schema",
-    "type": "doc",
-    "display": "hidden"
+  "config-schema": {
+    title: "Config Schema",
+    type: "doc",
+    display: "hidden",
   },
-  "demo":{
-    "title": "Request a Demo",
-    "type": "doc",
-    "display": "hidden"
+  demo: {
+    title: "Request a Demo",
+    type: "doc",
+    display: "hidden",
   },
-  "why-lamatic":{
-    "title": "Why Lamatic.ai",
-    "type": "doc",
-    "display": "hidden"
-  }
+  "why-lamatic": {
+    title: "Why Lamatic.ai",
+    type: "doc",
+    display: "hidden",
+  },
 };
--- pages/docs/nodes/_meta.ts
+++ pages/docs/nodes/_meta.ts
@@ -1,4 +1,16 @@
 export default {
+  "node-assistant": {
+    "title": "Node Assistant",
+    "type": "doc",
+  },
+  "node-logic":{
+    "title": "Node Logic",
+    "type": "doc",
+  },
+  "NODES TYPES":{
+    type: "separator",
+    title: "NODES TYPES",
+  },
   "ai": { "title": "AI", "type": "doc" },
   "apps": { "title": "Apps", "href": "/integrations/apps-data-sources" },
   "data": { "title": "Data", "type": "doc" },

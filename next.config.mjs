18ca223e3e10e6777885e0698f18894911359dda
akshatvirmani <akshatvirmani93@gmail.com>
2026-06-17 19:59:41 +0530

    Integrations: Add integrations pages GMAIL and SMTP

--- next.config.mjs
+++ next.config.mjs
@@ -155,7 +155,6 @@ const nonPermanentRedirects = [
   ["/new-issue", "https://github.com/Lamatic/docs/issues/new/choose"],
   ["/issues", "https://github.com/Lamatic/docs/issues"],
   ["/get-a-demo", "/docs/demo"],
-  ["/docs/whitelist-ip", "/docs/ips"],
   // ["/security", "/docs/data-security-privacy"],
   ["/idea", "https://github.com/Lamatic/docs/discussions/new?category=ideas"],
   [
@@ -411,11 +410,11 @@ const permanentRedirects = [
     ["/docs/nodes/apps/s3-node","/integrations/apps-data-sources/aws-s3"],
     ["/docs/nodes/apps/sharepoint-business-node","/integrations/apps-data-sources/sharepoint"],
     ["/docs/nodes/apps/slack-node","/integrations/apps-data-sources/slack"],
-    ["/docs/nodes/apps/smtp-node","/integrations/apps-data-sources"],
+    ["/docs/nodes/apps/smtp-node","/integrations/apps-data-sources/smtp"],
     ["/docs/nodes/apps/twilio-node","/integrations/apps-data-sources/twilio"],
     ["/docs/nodes/apps/web-search-node","/integrations/apps-data-sources/websearch"],
     ["/docs/nodes/apps/scraper-node","/integrations/apps-data-sources/firecrawl"],
-    ["/docs/nodes/apps/gmail-node","/integrations/apps-data-sources"],
+    ["/docs/nodes/apps/gmail-node","/integrations/apps-data-sources/gmail"],
     ["/docs/nodes/apps/cron-node","/integrations/apps-data-sources"],
 ["/docs/nodes/flow/query-database","/docs/flows/nodes"],
 ["/docs/vector_stores/vector_stores","/docs/context/vectordb"],
@@ -683,7 +682,8 @@ const permanentRedirects = [
 ["/docs/api-integration/sdk/next","/docs/sdk/next"],
 ["/docs/api-integration/sdk/react","/docs/sdk/react"],
 ["/docs/api-integration/integration-guide","/docs/sdk/integration-guide"],
-["/docs/docs-mcp","/docs/mcp/docs-mcp"]
+["/docs/docs-mcp","/docs/mcp/docs-mcp"],
+["/integrations/apps-data-sources/microsoft-sql-server","/integrations/apps-data-sources/mssql"]
 
 
 ];
@@ -706,7 +706,7 @@ const rewrites = [
   ["/.well-known", "/public/.well-known"],
   ["/robots.txt", "/public/robots.txt"],
     [
-      "/:path((?!docs|blog|guides|integrations|agentkits|templates|company|_next|public|assets|images|api|robots.txt|sitemap-0.xml|llms.txt|llms-full.txt|ambassadors).*)",
+      "/:path((?!docs|blog|guides|integrations|agentkits|templates|company|_next|public|assets|images|api|robots.txt|sitemap-0.xml|ambassadors).*)",
       "https://get.lamatic.ai/:path*",
     ],
 ];
